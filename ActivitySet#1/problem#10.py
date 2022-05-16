97a8e26b29478826bf3a1b03f257028e6b0d0bbe
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2022-05-16 15:32:00 +0000

    10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,22 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+x=dict()
+for line in handle:
+    if not line.startswith('From:'):
+        continue
+    words=line.split()
+    y =( words[1:])
+    for word in y:
+       x[word]=x.get(word,0) +1 
+       
+    largestcount = 0
+    largestword = 0
+    for word ,count in x.items() :
+        if largestcount is 0 or count > largestcount :
+            largestword=word
+            largestcount=count
+             
+        
+print(largestword,largestcount)
