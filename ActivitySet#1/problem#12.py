2e68f379ce86fd1398c4258d4e3f8a1052f64585
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2022-05-20 17:00:57 +0000

    12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,16 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+score = input("Enter Score: ")
+sc = float(score)
+if sc > 0.0 and sc < 1.0 :
+    
+    if sc >= 0.9:
+        print('A')
+    elif sc >= 0.8:
+        print('B')
+    elif sc >= 0.7:
+        print('C')
+    elif sc >= 0.6:
+        print('D')
+    else:
+        print('F')
+else :
+    print("enter valid marks within 0.0 to 1.0")
