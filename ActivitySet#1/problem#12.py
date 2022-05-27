ee13aa5432102278640aa5de66267a3bb57a15a3
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2022-05-27 16:19:11 +0000

    13

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -3,7 +3,7 @@ file = input('enter the file name')
 fhand=open(file)
 number = list()
 for line in fhand:
-  x = line.rstrip()
+  x =  line.rstrip()
   y= re.findall('[0-9]+',x)
   for a in y :
     a=int(y)
