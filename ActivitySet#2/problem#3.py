597946e0f2a6f93d5d91f7e3412d1f1536212753
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2022-06-02 16:26:38 +0000

    3

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,16 +1,19 @@
 
 
 def get_cs():
-    """get string input"""
-
-
-def cs_to_lot(cs):
+   x =input('enter the string')
+   return x 
+def cs_to_lot(cs,lst):
     """c onvert connected string to list of strings"""
+   a=cs.split(';')
+   fpr word in a :
+      lst.append()
+  return lst 
 
 
 def main():
     cs = get_cs()
-
+    lot = list()
     lot = cs_to_lot(cs)
     print(lot)
 
